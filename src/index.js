6a3b713d6ef0c31badd784c744867e14e730f2f1
zbyju <zbynek.jurica@gmail.com>
2022-09-16 20:20:51 +0300

    Add logging req body to morgan with post requests

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ const app = express();
 
 app.use(express.json());
 
-app.use(morgan("tiny"));
+morgan.token("body", function (req, res) {
+  if (req.method !== "POST") return " ";
+  return JSON.stringify(req.body);
+});
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let persons = [
   {
